c066f4575f060b845042c02aa7ec96abaecfa784
Anthony <anth06ny@gmail.com>
2023-02-13 11:26:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a2023_02_sup_parisa/exoKotlin.kt
@@ -0,0 +1,30 @@
+package com.example.a2023_02_sup_parisa
+
+fun main() {
+    println("HelloWorld")
+
+//    Créer une variable v1 de type String et y mettre la chaine "toto"
+    var v1 = "toto"
+    //    Afficher dans la console sa version en majuscule (uppercase)
+    println(v1.uppercase())
+//    Essayer d'y mettre la valeur null. (Erreur de compilation)
+//    Créer une variable v2 de type String nullable et y mettre la chaine "toto"
+    var v2 : String? = "toto"
+//    Afficher dans la console sa version en majuscule. Obligé de mettre le safe check
+    println(v2?.uppercase())
+
+//    Créer une variable v3 de type String nullable et y mettre la valeur null
+    var v3 :String? = null
+    //    Afficher dans la console sa version en majuscule
+    println(v3?.uppercase())
+
+    var v4 = v3 + v3
+    println(v4)
+
+    if(v1.isNotBlank()){
+
+    }
+    if(v3.isNullOrBlank()) {
+        println("v3 est bien null")
+    }
+}
